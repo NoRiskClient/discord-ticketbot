271d184188d80e34e00f4c781a0f08949f987699
enrico <71661047+enricoxD@users.noreply.github.com>
2026-05-17 10:55:09 +0200

    feat: add getRequiredRoleId hook to ICategory

--- src/main/java/eu/greev/dcbot/ticketsystem/categories/ICategory.java
+++ src/main/java/eu/greev/dcbot/ticketsystem/categories/ICategory.java
@@ -1,5 +1,6 @@
 package eu.greev.dcbot.ticketsystem.categories;
 
+import eu.greev.dcbot.utils.Config;
 import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
 import net.dv8tion.jda.api.interactions.modals.Modal;
 
@@ -18,5 +19,14 @@ public interface ICategory {
 
     default boolean isSensitive() {
         return false;
+    }
+
+    /**
+     * Role id required to open this category.
+     * {@code null} = no gate. A non-null value (including 0) means gated;
+     * 0 is treated as "disabled for everyone" by {@code TicketMenu.isEnabled}.
+     */
+    default Long getRequiredRoleId(Config config) {
+        return null;
     }
 }
